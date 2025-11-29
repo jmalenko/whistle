c40054465dba68003b4774b831e3c1e2d4344339
Jaromir Malenko <jaro@jaro.cz>
2025-11-29 14:11:05 +0100

    Shape

--- whistle.py
+++ whistle.py
@@ -1,65 +1,54 @@
 from build123d import *
 from ocp_vscode import show
 
-# Dimensions from drawing
+# Dimensions
 TOTAL_LENGTH = 48.0 # mm
-TOTAL_HEIGHT = 10.0
-LEFT_CIRCLE_DIAMETER = 10.0
-MIDDLE_CIRCLE_DIAMETER = 10.0
-STEP_WIDTH = 6.0
-STEP_HEIGHT = 0.7
-TOP_NOTCH_WIDTH = 13.0
-TOP_NOTCH_HEIGHT = 2.0
-RIGHT_NOTCH_WIDTH = 2.0
-RIGHT_NOTCH_HEIGHT = 1.0
-EXTRUSION_DEPTH = 20.0
+TOTAL_WIDTH = 10.0
+TOTAL_HEIGHT = TOTAL_WIDTH
 
-# Calculate positions
-left_circle_radius = LEFT_CIRCLE_DIAMETER / 2
-middle_circle_radius = MIDDLE_CIRCLE_DIAMETER / 2
-center_y = TOTAL_HEIGHT / 2
+WALL_THICKNESS = 1.2
+
+HOLE_WALL_THICKNESS = 2.0
+HOLE_DIAMETER = 10.0
+
+CUTOUT_HEIGHT = TOTAL_HEIGHT * 0.4
+CUTOUT_LENGTH = 6
+CUTOUT_OFFSET = 13
+
+TOOTH_OFFSET = 1
+TOOTH_HEIGHT = 0.8
+TOOTH_LENGTH = 2
 
 with BuildPart() as part:
     with BuildSketch() as sketch:
-        # Main body outline
-        with BuildLine() as outline:
-            # Start from bottom left of main rectangle
-            l1 = Line((left_circle_radius, 0), (TOTAL_LENGTH, 0))
-            l2 = Line(l1 @ 1, (TOTAL_LENGTH, TOTAL_HEIGHT))
-            l3 = Line(l2 @ 1, (left_circle_radius, TOTAL_HEIGHT))
-            l4 = Line(l3 @ 1, l1 @ 0)
-        
-        make_face()
-        
-        # Add left circle
-        with Locations((left_circle_radius, center_y)):
-            Circle(left_circle_radius)
-        
-        # Add middle circle (estimated position around 1/4 of length)
-        with Locations((TOTAL_LENGTH / 4, center_y)):
-            Circle(middle_circle_radius)
+        h2 = TOTAL_HEIGHT / 2
+        Circle(h2)
+
+        mouth = TOTAL_LENGTH - h2
+        Rectangle(mouth, TOTAL_HEIGHT, align=(Align.MIN, Align.CENTER))
+
+        Circle(h2 - HOLE_WALL_THICKNESS, mode=Mode.SUBTRACT)
+
+        with Locations((mouth - CUTOUT_OFFSET, h2)):
+            Polygon(
+                (0, 0), 
+                (0, -CUTOUT_HEIGHT), 
+                (-CUTOUT_LENGTH, 0),
+                align=(Align.MAX, Align.MAX), 
+                mode=Mode.SUBTRACT)
+
+        with Locations([
+            (mouth - TOOTH_OFFSET, h2),
+            (mouth - TOOTH_OFFSET, -h2+TOOTH_HEIGHT)
+        ]):
+            Rectangle(TOOTH_LENGTH, TOOTH_HEIGHT, 
+                align=(Align.MAX, Align.MAX), 
+                mode=Mode.SUBTRACT)
     
-    extrude(amount=EXTRUSION_DEPTH)
-    
-    # # Add step on top left
-    # with BuildSketch(Plane.XY.offset(EXTRUSION_DEPTH)):
-    #     with Locations((TOTAL_LENGTH - TOP_NOTCH_WIDTH - STEP_WIDTH, TOTAL_HEIGHT)):
-    #         Rectangle(STEP_WIDTH, STEP_HEIGHT, align=(Align.MIN, Align.MIN))
-    # extrude(amount=-STEP_HEIGHT)
-    
-    # # Add top notch
-    # with BuildSketch(Plane.XY.offset(EXTRUSION_DEPTH)):
-    #     with Locations((TOTAL_LENGTH - TOP_NOTCH_WIDTH, TOTAL_HEIGHT)):
-    #         Rectangle(TOP_NOTCH_WIDTH, TOP_NOTCH_HEIGHT, align=(Align.MIN, Align.MIN))
-    # extrude(amount=-TOP_NOTCH_HEIGHT)
-    
-    # # Add right side notch
-    # with BuildSketch(Plane.XY.offset(EXTRUSION_DEPTH)):
-    #     with Locations((TOTAL_LENGTH - RIGHT_NOTCH_WIDTH, TOTAL_HEIGHT - RIGHT_NOTCH_HEIGHT)):
-    #         Rectangle(RIGHT_NOTCH_WIDTH, RIGHT_NOTCH_HEIGHT, align=(Align.MIN, Align.MIN))
-    # extrude(amount=-RIGHT_NOTCH_HEIGHT)
+    extrude(amount=TOTAL_WIDTH)
+
 
 export_step(part.part, "whistle.step")
 
-show(part)
+show(part, reset_camera=False)
 
